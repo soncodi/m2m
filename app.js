1bcb0f746e1b2da4840d1a3f8d8257d630ed8374
Alex Soncodi <asoncodi@gmail.com>
2013-09-14 14:47:00 -0500

    Websocket ping/pong.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 var port = 8080;
 
 app.use(express.static(__dirname + '/static'));
+
+app.get('*', function(req, res) {
+  return res.sendfile(__dirname + '/static/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+  socket.on('ping', function(data) {
+    console.log(data);
+
+    socket.emit('pong', {
+      pong: new Date()
+    });
+  });
+});
 
 app.get('/api', function(req, res, next) {
   return res.json({
@@ -10,6 +26,6 @@ app.get('/api', function(req, res, next) {
   });
 });
 
-app.listen(port, function() {
+server.listen(port, function() {
   console.log('up on %d', port);
 });
