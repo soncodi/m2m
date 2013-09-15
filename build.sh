559c33835bb60bcc5b0ebf86a973d415115c0b4a
Alex Soncodi <asoncodi@gmail.com>
2013-09-15 14:09:59 -0500

    Add splashscreens.

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 rm -rf platforms/android
+rm -rf plugins/*
+
+phonegap build android
+
+cp -R www/img/res/* platforms/android/res
+
+echo "Installing SplashScreen"
+cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-splashscreen.git
 
 phonegap run android
