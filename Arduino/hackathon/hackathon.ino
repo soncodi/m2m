be3613320323a9d89820c65843def96c0228dffa
Stefan Negrea <snegrea@gmail.com>
2013-09-16 15:32:56 -0500

    Changes to the code to match a new account and improved functionality.

--- Arduino/hackathon/hackathon.ino
+++ Arduino/hackathon/hackathon.ino
@@ -2,9 +2,9 @@
 #include "Timer.h"
 
 // Longest variable is 32 chars
-PROGMEM const prog_uchar  M2MIO_USERNAME[]    = "team7@att.com";
-PROGMEM const prog_uchar  M2MIO_PASSWORD[]    = "a04f81faf54465382e71526874920e26";    // MD5 key of password
-PROGMEM const prog_uchar  M2MIO_DOMAIN[]      =  "69bd3ed2f51a8d7bd68180eda5d0c2c8";
+PROGMEM const prog_uchar  M2MIO_USERNAME[]    = "asoncodi@gmail.com";
+PROGMEM const prog_uchar  M2MIO_PASSWORD[]    = "3dbcf1dd4d06937acec56583b6f8f1b7";    // MD5 key of password
+PROGMEM const prog_uchar  M2MIO_DOMAIN[]      =  "1ce215c3f9414890642cbc67595780a7";
 PROGMEM const prog_uchar  M2MIO_DEVICE_TYPE[] =	 "arduino";
 PROGMEM const prog_uchar  M2MIO_DEVICE_ID[]   =  "device01";
 
@@ -25,9 +25,7 @@ int moistureSensorPin = 4;
 void cmdCallBack(char *topic, uint8_t* payload, unsigned int len);
 
 void setup()
-{
-  pinMode(13, OUTPUT);
-  
+{  
   while(!Serial);
   delay(1500);
 
@@ -36,16 +34,21 @@ void setup()
 
   c = ATT3GModemClient();
   acc = ATTCloudClient(cmdCallBack,c);
-
+  
+  Serial.print("Connected: ");
+  Serial.println(acc.connected());
+  
   if (!acc.connect(M2MIO_DEVICE_ID,M2MIO_USERNAME,M2MIO_PASSWORD))
   {
    Serial.println(F("Unable to connect to network (12)"));
   }
 
   acc.setDomainStuffThing(M2MIO_DOMAIN,M2MIO_DEVICE_TYPE,M2MIO_DEVICE_ID);
-
   //acc.registerForCommands();
+  //acc.subscribe("1ce215c3f9414890642cbc67595780a7/arduino/device02");
   
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
   int sensorEvent = t.every(REPORTING_INTERVAL_MS, getSensorValue);
 }
 
@@ -56,14 +59,13 @@ void loop()
   t.update();
 }
 
-void cmdCallBack(char *topic, uint8_t* payload, unsigned int len)
-{
+void cmdCallBack(char *topic, uint8_t* payload, unsigned int len) {
   Serial.println(F("In the cmdCallBack()"));
   Serial.println((char*)payload);
 }
 
 void getSensorValue()
-{
+{  
   Serial.println("getSensorValue");
   // temp
   int B=3975;
@@ -73,18 +75,19 @@ void getSensorValue()
   float dat=1/(log(resistance/10000)/B+1/298.15)-273.15;
   dat = dat * 1.8 + 32;
   
+  int fanOn = 0;
   if ((int)dat > 75)
   {
     digitalWrite(13, HIGH);
     delay(500);
-    acc.sendKV("Fan", "ON");
+    fanOn = 1;
     Serial.println("Fan on");
   }
   else
   {
     digitalWrite(13, LOW);
     delay(500);
-    acc.sendKV("Fan", "OFF");
+    fanOn = 0;
     Serial.println("Fan off");
   }
     
@@ -110,6 +113,22 @@ void getSensorValue()
   else
     frac = (int(dat)- dat ) * 100;
     
+  int lightsOn = 0;
+  if (dig1  > 15)
+  {
+    digitalWrite(12, HIGH);
+    delay(500);
+    lightsOn = 1;
+    Serial.println("Lights on");
+  }
+  else
+  {
+    digitalWrite(12, LOW);
+    delay(500);
+    lightsOn = 0;
+    Serial.println("Lights off");
+  }
+    
   char light[10];
   sprintf(light, "%d.%d", dig1, frac);
   Serial.print("Light sensor resistance: ");
@@ -123,10 +142,18 @@ void getSensorValue()
   Serial.print("Moisture: ");
   Serial.println(moisture);
   
-  acc.sendKV("Temperature", temp);
-  acc.sendKV("Light", light);
-  acc.sendKV("Moisture", moisture);
+  acc.startMessage();
+  acc.addKVToMessage("Temperature", temp);
+  acc.addKVToMessage("Light", light);
+  acc.addKVToMessage("Moisture", moisture);
+  acc.addKVToMessage("FanStatus", fanOn);
+  acc.addKVToMessage("LightStatus", lightsOn);
+  acc.endMessage();
+  boolean q = acc.sendMessage();
   
-  delay(500);
+  Serial.println(q);
+  
+  Serial.println(acc.getMessage());
+  
+  delay(500);  
 }
-
