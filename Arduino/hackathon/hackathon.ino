aac3ed84430fdc90a2e83760dc425b3a241b6996
Martin Tran <bigmarty@gmail.com>
2013-09-16 22:25:12 -0500

    Update the Arduino code to control the lights and fan from the 2letmetry.

--- Arduino/hackathon/hackathon.ino
+++ Arduino/hackathon/hackathon.ino
@@ -21,6 +21,9 @@ int tempSensorPin = 0;
 int lightSensorPin = 1;
 int moistureSensorPin = 4;
 
+int lightsOn = 0;
+int fanOn = 0;
+
 float Res0=20.0;              // Resistance in the circuit of sensor 0 (KOhms)
 // depending of the Resistance used, you could measure better at dark or at bright conditions.
 // you could use a double circuit (using other LDR connected to analog pin 1) to have fun testing the sensors.
@@ -52,7 +55,11 @@ void setup()
   //acc.subscribe("1ce215c3f9414890642cbc67595780a7/arduino/device02");
   
   pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+
   pinMode(13, OUTPUT);
+  digitalWrite(12, LOW);
+
   int sensorEvent = t.every(REPORTING_INTERVAL_MS, getSensorValue);
 }
 
@@ -66,39 +73,37 @@ void cmdCallBack(char *topic, uint8_t* payload, unsigned int len)
 {
   Serial.println(F("Command Received"));
 
-  if (acc.commandCompare("{\"light\":\"on\"}", payload, len)) {
+  if (acc.commandCompare("{\"f\":\"1\"}", payload, len)) {
+    Serial.println(F("Received fan on"));
+    digitalWrite(12, HIGH);
+    lightsOn = 1;
+  } else if (acc.commandCompare("{\"f\":\"0\"}", payload, len)) {
+    Serial.println(F("Received fan off"));
+    digitalWrite(12, LOW);
+    lightsOn = 0;
+  } else if (acc.commandCompare("{\"l\":\"1\"}", payload, len)) {
+    Serial.println(F("Received light on"));
     digitalWrite(13, HIGH);
-  } else if (acc.commandCompare("{\"light\":\"off\"}", payload, len)) {
+    fanOn = 1;
+  } else if (acc.commandCompare("{\"l\":\"0\"}", payload, len)) {
+    Serial.println(F("Received light off"));
     digitalWrite(13, LOW);
+    fanOn = 0;
   } else {
-    Serial.println(F("No match"));
+    Serial.println((char*)payload);
   }
 }
 
 void getSensorValue()
 {  
   Serial.println(F("getSensorValue"));
-  // temp
+
   int B=3975;
   int val = analogRead(tempSensorPin);
   
   float resistance=(float)(1023-val)*10000/val; 
   float dat=1/(log(resistance/10000)/B+1/298.15)-273.15;
   dat = dat * 1.8 + 32;
-  
-  int fanOn = 0;
-  if ((int)dat > 75)
-  {
-    digitalWrite(12, HIGH);
-    fanOn = 1;
-    Serial.println(F("Fan on"));
-  }
-  else
-  {
-    digitalWrite(12, LOW);
-    fanOn = 0;
-    Serial.println(F("Fan off"));
-  }
     
   unsigned int dig1 = int(dat);
   unsigned int frac;
@@ -121,21 +126,7 @@ void getSensorValue()
   Serial.print(F("Luminosity: "));
   Serial.print(lux0);
   Serial.println(F(" Lux"));
-    
-  int lightsOn = 0;
-  if (lux0 < 50)
-  {
-    digitalWrite(13, HIGH);
-    lightsOn = 1;
-    Serial.println(F("Lights on"));
-  }
-  else
-  {
-    digitalWrite(13, LOW);
-    lightsOn = 0;
-    Serial.println(F("Lights off"));
-  }
-  
+ 
   // moisture sensor
   val = analogRead(moistureSensorPin);
   
